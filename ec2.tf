c3dae8d570f00915a530ea3907d766cbc5ddc22a
montfortthomas <85698949+montfortthomas@users.noreply.github.com>
2023-04-22 14:22:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,10 @@
-resource "aws_instance" "Public_terraform" {
-  ami           = "ami-0e742cca61fb65051"
-  instance_type = "t2.medium"
-  key_name      = "0602"
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIAXWFQBKZWGG5OC3XS"
+  secret_key = "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"
+}
+
+resource "aws_instance" "myec2" {
+   ami = "ami-074dc0a6f6c764218"
+   instance_type = "t2.micro"
 }
