a1d46ab2f668192d5259b4cfec92357fb1364edc
montfortthomas <85698949+montfortthomas@users.noreply.github.com>
2023-04-22 14:38:53 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,10 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAXWFQBKZWGG5OC3XS"
-   secret_key = "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"
-}
+  
+  avccess_key = "AKIAXWFQBKZWGG5OC3XS"
+  secret_key = "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"   
+  }
+
 
 resource "aws_instance" "myec2" {
    ami = "ami-06fc49795bc410a0c"
